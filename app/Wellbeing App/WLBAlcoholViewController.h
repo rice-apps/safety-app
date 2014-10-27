3432deb2d6cc2aa83b66fa928e0a89fa3c6a8e2d
Leo Du <leoyilindu.dev@gail.com>
2014-10-27 01:15:04 -0500

    Alcohol View added

---
+++ app/Wellbeing App/WLBAlcoholViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLBAlcoholViewController.h
+//  Wellbeing App
+//
+//  Created by Leo Du on 10/27/14.
+//  Copyright (c) 2014 Student Association. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLBAlcoholViewController : UIViewController
+
+@end
