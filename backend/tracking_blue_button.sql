0ddcef9d557741dfe0ef29a47fbc56cee5bb3537
ricebrianlee <brianlee.jpg@gmail.com>
2015-10-03 16:01:25 -0500

    what

--- backend/tracking_blue_button.sql
+++ backend/tracking_blue_button.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS tracking;
+DROP TABLE IF EXISTS tracking_blue_button;
 
 CREATE TABLE tracking_blue_button(
-	UUID TEXT,
-	net_id TEXT,
-	request_id TEXT,
+  request_id TEXT,
+  UUID TEXT,
+  net_id TEXT,
   longitude REAL,
   latitude REAL,
   time TEXT,
---
+++ backend/tracking_escort.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS tracking_escort;
+
+CREATE TABLE tracking_escort(
+  requestID TEXT,
+  UUID TEXT,
+  netID TEXT,
+  longitude REAL,
+  latitude REAL,
+  time TEXT,
+  resolved INT
+);
