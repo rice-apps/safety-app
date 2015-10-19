24c7f6b63c91128f176f6f4acaedfa9106f4cbcb
jeffreyxiong <jeffxiong017@gmail.com>
2015-10-18 19:30:54 -0500

    swift things

---
+++ app/Wellbeing App/WLBFirstViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WLBFirstView.swift
+//  Wellbeing App
+//
+//  Created by Jeffrey Xiong on 10/18/15.
+//  Copyright © 2015 Student Association. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WLBFirstViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    func on
+}
