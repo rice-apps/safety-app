52875822651635578a326ede1021e5b00cf6e644
jeffreyxiong <jeffxiong017@gmail.com>
2015-10-18 20:28:34 -0500

    project setup

---
+++ app/Wellbeing/Wellbeing/NumbersTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Wellbeing
+//
+//  Created by Jeffrey Xiong on 10/18/15.
+//  Copyright © 2015 Rice Apps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NumbersTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
