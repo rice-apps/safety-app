a07511033374ff642c75e4f0e16aa1fd9ae5b195
jeffreyxiong <jeffxiong017@gmail.com>
2015-10-20 12:35:21 -0500

    finished numbers-view

--- app/Wellbeing/Wellbeing/NumbersTableViewController.swift
+++ app/Wellbeing/Wellbeing/NumbersTableViewController.swift
@@ -11,21 +11,78 @@ import UIKit
 
 class NumbersTableViewController: UITableViewController{
     
+    // Class Variables
+    
+    let numberCellID = "numberCell"
     var numbers = [String]()
     var organizations = [String]()
-    var _receivedData = NSMutableData()
+    lazy var _jsonData = NSDictionary()
     
-
+    // Generic View
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeRequest()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // URL Request
+    
+    func makeRequest(){
+        let path: String = "http://riceapps.org:19125/api/numbers"
+        let url: NSURL = NSURL(string: path)!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url){
+            (data, response, error) -> Void in
+            do {
+                // Load JSON Object
+                self._jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                
+                // Store relevant entries
+                let loadedNumbers = self._jsonData["result"] as! NSArray
+                for entry in loadedNumbers{
+                    self.numbers.append(entry["number"] as! String)
+                    self.organizations.append(entry["name"] as! String)
+                }
+                self.tableView.reloadData()
+                
+            } catch _ {
+                // Error
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    // Table View
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(String(self.organizations))
+        return self.organizations.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.numberCellID, forIndexPath: indexPath)
+        
+        cell.textLabel?.text = self.organizations[indexPath.row]
+        cell.detailTextLabel?.text = self.numbers[indexPath.row]
+        
+        return cell
+    }
+    
+    
 
 }
 
