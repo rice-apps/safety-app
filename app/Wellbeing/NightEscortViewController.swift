f877f771d046c61c8b42f9067b62f9b70939de0c
Xilin Liu <xal1@rice.edu>
2015-11-22 20:51:10 -0600

    added hidden view that shows upon login

--- app/Wellbeing/NightEscortViewController.swift
+++ app/Wellbeing/NightEscortViewController.swift
@@ -8,9 +8,22 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class NightEscortViewController: UIViewController {
 
+    @IBOutlet weak var trackingMap: MKMapView!
+    @IBOutlet weak var logButton: UIBarButtonItem!
+    var loggedIn: Bool {
+        get {
+            return !trackingMap.hidden
+        }
+        set {
+            trackingMap.hidden = !newValue
+            logButton.title = newValue ? "Log Out" : "Log In"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +34,36 @@ class NightEscortViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func logInOrOut(sender: UIBarButtonItem) {
+        if loggedIn {
+            loggedIn = false
+        } else {
+            promptLogin()
+        }
+    }
+    
+    func promptLogin() {
+        let alert = UIAlertController(title: "Login", message: "Please login with the provided password", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "Login", style: .Default) {
+            (action: UIAlertAction) -> Void in
+            
+            if let tf = alert.textFields?.first {
+                if tf.text == "jeffrey" {
+                    self.loggedIn = true
+                } else {
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+            })
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel) {
+            (action: UIAlertAction) -> Void in
+            // do nothing
+            })
+        
+        alert.addTextFieldWithConfigurationHandler { (textField) in textField.placeholder = "Password" }
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
-
