35421f8e8ce75c28c13fd51760a98095083a9509
jeffreyxiong <jeffxiong017@gmail.com>
2016-01-24 20:58:19 -0600

    minor changes

--- app/Wellbeing/ReportingViewController.swift
+++ app/Wellbeing/ReportingViewController.swift
@@ -27,8 +27,8 @@ class ReportingViewController: UIViewController {
     
     func postRequest(content: String) {
         let postString = "description=" + content.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
-        
-        let path: String = "http://riceapps.org:19125/api/anon_reporting"
+        // actual path: http://riceapps.org/api/anon_reporting
+        let path: String = "localhost:5000/api/anon_reporting"
         let url: NSURL = NSURL(string: path)!
         let cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
         let request = NSMutableURLRequest(URL: url, cachePolicy: cachePolicy, timeoutInterval: 2.0)
@@ -66,6 +66,7 @@ class ReportingViewController: UIViewController {
     
     @IBAction func submitReport(sender: AnyObject) {
         postRequest(reportTextView.text)
+        print("report submitted")
     }
     
     override func didReceiveMemoryWarning() {
