5068858839586fd8e6938c0379b184026847c44b
jeffreyxiong <jeffxiong017@gmail.com>
2016-01-31 20:29:02 -0600

    blue button sql update

--- backend/tracking_blue_button.sql
+++ backend/tracking_blue_button.sql
@@ -2,16 +2,17 @@ DROP TABLE IF EXISTS tracking_blue_button;
 
 CREATE TABLE tracking_blue_button(
   requestID INTEGER PRIMARY KEY,
-  deviceID INTEGER,
-  UUID TEXT,
+  caseID TEXT,
+  deviceID TEXT,
   longitude REAL,
   latitude REAL,
   date TEXT,
   resolved INTEGER
 );
 
-INSERT INTO tracking_blue_button(deviceID, UUID, longitude, latitude, date, resolved)
-  VALUES (10, "phone1", 29.716909, -95.401594, "2015-10-25 19:38:50", 0);
 
-INSERT INTO tracking_blue_button(deviceID, UUID, longitude, latitude, date, resolved)
-  VALUES (11, "phone2", 29.716909, -95.401594, "2015-10-25 17:38:22", 0);
+--INSERT INTO tracking_blue_button(caseID, deviceID, longitude, latitude, date, resolved)
+--  VALUES (10, "phone1", 29.716909, -95.401594, "2015-10-25 19:38:50", 0);
+--
+--INSERT INTO tracking_blue_button(caseID, deviceID, longitude, latitude, date, resolved)
+--  VALUES (11, "phone2", 29.716909, -95.401594, "2015-10-25 17:38:22", 0);
